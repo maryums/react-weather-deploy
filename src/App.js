ace56cc2dfc536aa20f974116d0c9c68d4e1a1ba
maryums <maryum@gmail.com>
2022-02-27 17:14:45 -0600

    error state added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
 import axios from 'axios';
+
+import Error from "./components/Error"
 
 
 function App() {
 
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
-
-
+  const [errorMessage, setErrorMessage] = useState('');
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=f9b8f25a74c92adeeb7d4391ccb86814`
+  const errorMsg = '';
 
   const searchLocation = (event) => {
+
     if (event.key === 'Enter') {
       axios.get(url).then(
         (response) => {
           setData(response.data)
-          console.log(response.status);
-          console.log(response.data);
         })
         .catch(error => {
-          console.log(error.response.data.message);
-          const errorText = error.response.data.message;
-          console.log(error.response.data);
-          document.querySelector("#citynotfound").innerHTML = errorText;
+          const errorMsg = error.response.data.message;
+          setErrorMessage(error.response.data.message);
         })
     }
   }
+
+  const handleReset = (e) => {
+    // e.preventDefault();
+    console.log(location, data);
+    // clearing the values
+    setLocation("");
+    setData({});
+    setErrorMessage("");
+  };
+
 
   return (
     <div className="App">
@@ -36,35 +46,34 @@ function App() {
           onChange={event => setLocation(event.target.value)}
           onKeyPress={searchLocation}
           placeholder='Enter Location'
+          onFocus={handleReset}
           type="text" />
+
+
+
       </div>
 
       <div className="container">
-
         <div className="top">
 
-          <div className="top-error"> <h1 id="citynotfound"></h1></div>
 
           <div className="location">
             <p>{data.name}</p>
           </div>
+
           <div className="temp">
             {data.main ? <h1>{data.main.temp.toFixed()} F</h1> : null}
-
           </div>
+
           <div className="description">
             {data.weather ? <p>{data.weather[0].main} </p> : null}
           </div>
-
         </div>
-
 
         {data.name !== undefined &&
           <div className="bottom">
-
             <div className="feels">
-              <p className="bold">
-                {data.main ? <p>{data.main.feels_like.toFixed()} </p> : null}</p>
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()} </p> : null}
               <p>Feels Like </p>
             </div>
             <div className="humidity">
@@ -78,6 +87,15 @@ function App() {
 
           </div>
         }
+
+        {errorMessage && (
+          <h3 className="error center"> {errorMessage} </h3>
+        )
+        }
+
+
+
+
       </div>
     </div>
   );
---
+++ src/components/Error.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Error = (location, data, setData, setLocation) => {
+
+    console.log(location)
+    return (
+        <div> <h3> This City Isnt Found </h3></div>
+    )
+
+
+
+}
+
+
+
+export default Error;
